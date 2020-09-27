80e3ce480120d4a85085f79b0ccc82a46e32b244
sahanpasindu <sahanpasindu07@gmail.com>
2020-09-27 19:26:39 +0530

    Tutorial 06 Video Project (Flutter Sinhala Tutorial #6 - AppBar - Episode 01)

---
+++ flutter_tutorial_06_appbar_1/android/app/src/main/kotlin/com/ccsl/flutter_tutorial_06_appbar_1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ccsl.flutter_tutorial_06_appbar_1
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
---
+++ flutter_tutorial_06_appbar_1/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      DevicePreview(
+        builder: (context) => MyApp(),
+      ),
+    );
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      locale: DevicePreview.of(context).locale, // <--- Add the locale
+      builder: DevicePreview.appBuilder, // <--- Add the builder
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter CCSL'),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {},
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: () {},
+          ),
+        ],
+        flexibleSpace: Image.asset(
+          "assests/back.jpg",
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
